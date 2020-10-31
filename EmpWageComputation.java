a65d8654d04c2f382a7c5a0cb7bf12bae941c357
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-31 09:38:40 +0530

    Added:calculate Wage till condition of total working days or hours reached for a month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,26 +1,26 @@
 public class EmpWageComputation {
-	public static void main(String[] args){
-		System.out.println("Welcome to Employee Wage Computation.");
-	}
+        public static void main(String[] args){
+                System.out.println("Welcome to Employee Wage Computation.");
+        }
 }
 //Check Employee is Present or absent
 public class EmpWageComputation {
-	public static void main(String[] args){
-		System.out.println("Welcome to Employee Wage Computation.");
-		// Constants
-		int IS_FULL_TIME = 1;
-		// Computation
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME){
-			System.out.println("Employee is Present");
-			}
-		else{
-			System.out.println("Employee is Absent");
-		     }
+        public static void main(String[] args){
+                System.out.println("Welcome to Employee Wage Computation.");
+                // Constants
+                int IS_FULL_TIME = 1;
+                // Computation
+                double empCheck = Math.floor(Math.random() * 10) % 2;
+                if (empCheck == IS_FULL_TIME){
+                        System.out.println("Employee is Present");
+                        }
+                else{
+                        System.out.println("Employee is Absent");
+                     }
 }
 }
 
-//Calculate Daily Employee Wage 
+//Calculate Daily Employee Wage
 public class EmpWageComputation{
 
    public static void main(String[ ] args) {
@@ -176,3 +176,8 @@ public class EmpWageComputation{
 
 }
 }
+
+
+
+
+
