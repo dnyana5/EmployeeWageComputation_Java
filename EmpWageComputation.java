cac21c0b1d9d0a2e9ef4eb0e1c52b6047d61e895
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-31 08:33:58 +0530

    Added:calculate Monthly Employee Wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -102,3 +102,39 @@ public class EmpWageComputation{
 
 }
 }
+//Calculate Monthly Employee Wage
+public class EmpWageComputation{
+      public static final int IS_PART_TIME = 0;
+      public static final int IS_FULL_TIME = 1;
+      public static final int EMP_RATE_PER_HOUR = 20;
+      public static final int empPresent = 1;
+      public static final int NUM_OF_WORKING_DAYS = 20;
+
+   public static void main(String[ ] args) {
+            //variables
+      int empHrs = 0, empWage = 0, totalEmpWage =0;
+      if (empPresent == (int) (Math.random() * 10) % 2)
+      {
+         System.out.println("Employee is Present");
+         for (int day =0; day < NUM_OF_WORKING_DAYS; day++){
+         int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+         switch (empCheck) {
+            case IS_PART_TIME:
+               empHrs = 4;
+               break;
+            case IS_FULL_TIME:
+               empHrs = 8;
+               break;
+         default:
+            empHrs = 0;
+         }
+         empWage = empHrs * EMP_RATE_PER_HOUR;
+         totalEmpWage += empWage;
+         System.out.println("Emp Wage:" + empWage);
+      }System.out.println("Total Emp Wage For Month:" + totalEmpWage);
+}
+      else
+          System.out.println("Employee is Absent");
+
+}
+}
