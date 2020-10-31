be8828dd67a2092416e5adbe9507bf984377e8ee
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-31 09:51:47 +0530

    Added:Compute Employee wage for multiple company

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -222,3 +222,47 @@ public class EmpWageComputation
       }
 }
 
+//For Multiple Company 
+public class EmpWageComputation
+{
+      public static final int IS_PART_TIME = 0;
+      public static final int IS_FULL_TIME = 1;
+
+
+   public static int computeEmpWage(String company, int EMP_RATE_PER_HOUR,int NUM_OF_WORKING_DAYS,int MAX_HRS_IN_MONTH)
+   {
+            //variables
+      int empHrs = 0, totalEmpHrs = 0, totalWorkingDays =0;
+               while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+               {
+                  totalWorkingDays++;
+                  int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+                  switch (empCheck)
+                     {
+                        case IS_PART_TIME:
+                           empHrs = 4;
+                           break;
+                        case IS_FULL_TIME:
+                           empHrs = 8;
+                           break;
+                        default:
+                           empHrs = 0;
+                     }
+                  totalEmpHrs += empHrs;
+                  System.out.println("Days:" + totalWorkingDays + "  Emp Hr:" +empHrs);
+               }
+                  int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+                  System.out.println("Total Emp Wage for company::"+company+" is:"+totalEmpWage);
+                  return totalEmpWage;
+         }
+
+
+
+      public static void main(String[] args)
+      {
+         computeEmpWage("samsung",20,3,10);
+         computeEmpWage("Apple",15,5,25);
+      }
+}
+
+
