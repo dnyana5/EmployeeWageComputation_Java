01a26b52a11d04128c60d7e8ee393505c77a590c
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-31 07:59:15 +0530

    Checking Employee is Present or Absent

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,7 +1,21 @@
 public class EmpWageComputation {
-
-   public static void main(String[ ] args) {
-        System.out.println("Welcome to employee Wage Computation Program on master branch");
-   }
+	public static void main(String[] args){
+		System.out.println("Welcome to Employee Wage Computation.");
+	}
 }
-
+//Check Employee is Present or absent
+public class EmpWageComputation {
+	public static void main(String[] args){
+		System.out.println("Welcome to Employee Wage Computation.");
+		// Constants
+		int IS_FULL_TIME = 1;
+		// Computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME){
+			System.out.println("Employee is Present");
+			}
+		else{
+			System.out.println("Employee is Absent");
+		     }
+}
+}
