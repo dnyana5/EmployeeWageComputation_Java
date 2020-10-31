2fe21bdf835419f95972b9de687b3e85e24dbfe0
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-31 07:47:28 +0530

    Welcome Message

---
+++ EmpWageComputation.java
@@ -0,0 +1,7 @@
+public class EmpWageComputation {
+
+   public static void main(String[ ] args) {
+        System.out.println("Welcome to employee Wage Computation Program on master branch");
+   }
+}
+
