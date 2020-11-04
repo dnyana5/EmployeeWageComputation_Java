40895308546d70230d598050b2f9114b3f221241
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-11-04 13:15:09 +0530

    Added:save total of company

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -266,3 +266,63 @@ public class EmpWageComputation
 }
 
 
+//UC9-save total wage of company
+import java.util.Scanner;
+public class EmpWageComputation {
+        public static final int IS_PART_TIME = 1;
+        public static final int IS_FULL_TIME = 2;
+
+        private String company;
+        private int wagePerHour;
+        private int workingDays;
+        private int workingHours;
+        private int totalEmpWage;
+
+        public EmpWageComputation(String company ,int wagePerHour,int workingDays,int workingHours){
+                this.company=company;
+                this.wagePerHour=wagePerHour;
+                this.workingDays=workingDays;
+                this.workingHours=workingHours;
+        }
+
+        //Calculating monthly wage of employee
+        public void employeeMonthlyWage(){
+                //variables
+                int empHours=0;
+                int totalWorkingDays=0, totalEmpHours=0;
+                int empCheck=(int) Math.floor(Math.random() * 10) % 3;
+                while (totalEmpHours <= workingHours && totalWorkingDays < workingDays){
+                        totalWorkingDays++;
+                        switch(empCheck){
+                                case IS_PART_TIME:
+                                        empHours=8;
+                                        break;
+                                case IS_FULL_TIME:
+                                        empHours=4;
+                                        break;
+                                default:
+                                        empHours=0;
+                        }
+                totalEmpHours += empHours;
+                }
+        System.out.println("total employee working hour - "+totalEmpHours);
+        System.out.println("total employee working days - "+totalWorkingDays);
+        totalEmpWage = totalEmpHours * wagePerHour;
+        }
+
+        public String toString(){
+                return "Total employee wage for company : "+company+" is - "+totalEmpWage;
+        }
+
+        public static void main(String[] args){
+                System.out.println("Welcome to Employee Wage Computation");
+                //take user input for each company
+                EmpWageComputation samsung = new EmpWageComputation("Samsung",20,20,100);
+                EmpWageComputation apple = new EmpWageComputation("Apple",10,25,150);
+                samsung.employeeMonthlyWage();
+                System.out.println(samsung);
+                apple.employeeMonthlyWage();
+                System.out.println(apple);
+        }
+}
+
